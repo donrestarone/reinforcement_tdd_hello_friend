39a24d0bd0ddea0d1b48b1e77c338ff83b6c6344
donrestarone <shashikejayatunge@gmail.com>
2018-05-02 13:26:18 -0400

    finish exercise

--- friend.rb
+++ friend.rb
@@ -1,11 +1,12 @@
 class Friend 
 	
-	def greet(name_of_friend = "")
-		return "hello #{name_of_friend}"
-	end 
-
-
-	
+	def greet(name_of_friend = nil)
+		if name_of_friend
+			return "Hello #{name_of_friend}!"
+		else 
+			return "Hello !"
+		end
+	end
 end 
 
 
