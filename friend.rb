98e97903915888969eb70c28ee3e16f49616b728
donrestarone <shashikejayatunge@gmail.com>
2018-05-02 13:18:45 -0400

    start assignment, 1 test passing

---
+++ friend.rb
@@ -0,0 +1,19 @@
+class Friend 
+	
+	def greet(name_of_friend = "")
+		return "hello #{name_of_friend}"
+	end 
+
+
+	
+end 
+
+
+
+	#experimenting
+# def hello(name = "")
+# 	return "hello #{name}"
+# 	# "hello"
+# end 
+
+# p hello('bob')
